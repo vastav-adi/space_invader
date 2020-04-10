6a7db6df1c0fd864656797f49b1a3673692045bc
vastav-adi <vastavadi2018@gmail.com>
2020-04-10 21:18:11 +0530

    added player icon

--- main.py
+++ main.py
@@ -12,14 +12,27 @@ icon = pygame.image.load("logos/logo.png")
 pygame.display.set_caption("ASS INVADERS")
 pygame.display.set_icon(icon)
 
+# player
+playerImg = pygame.image.load("user_image/user_bl.png")
+playerX = 370
+playerY = 480 
+
+
+# draws the player
+def player():
+	screen.blit(playerImg,(playerX,playerY))
+
+
 #game loop
 running = True
 while running:
+	# RGB
+	screen.fill((150,255,255))
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
 
-
-	# RGB
-	screen.fill((255,0,0))
+	# calls the function to draw the player over the screen
+	player()
 	pygame.display.update()
