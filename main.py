1eeb6877ab13f74bb6f917a5acfc5928e17d8530
vastav-adi <vastavadi2018@gmail.com>
2020-04-10 21:00:22 +0530

    added logo, fill and update

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ pygame.init()
 # creating screen
 screen = pygame.display.set_mode((800,600))
 
+# title and Icon
+icon = pygame.image.load("logos/logo.png")
+
+pygame.display.set_caption("ASS INVADERS")
+pygame.display.set_icon(icon)
+
 #game loop
 running = True
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+
+
+	# RGB
+	screen.fill((255,0,0))
+	pygame.display.update()
